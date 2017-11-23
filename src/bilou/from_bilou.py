3ae77f4a10c8c613d504019f4c8569047eb22fc2
lilaspourpre <nia@ispras.ru>
2017-11-23 11:43:34 +0300

    from bilou changed competely comparing to the old version

--- src/bilou/from_bilou.py
+++ src/bilou/from_bilou.py
@@ -1,107 +1,57 @@
 # -*- coding: utf-8 -*-
-import codecs
 import logging
 
 
-class FromBILOU:
-    def __init__(self):
-        self.dic_of_files_of_results = {}
+def untag(list_of_tags, list_of_tokens):
+    """
+    :param list_of_tags:
+    :param list_of_tokens:
+    :return:
+    """
+    if len(list_of_tags) == len(list_of_tokens):
+        dict_of_final_ne = {}
+        ne_words = []
+        ne_tag = None
 
-    def untag(self, dict_of_data, dict_of_tags):
-        for file in dict_of_data.keys():
-            dic_of_results = {}
-            ne_tokens = []
-            prev_tag = None
-            token_position_dic = {dict_of_data[file][key][0]: key for key in dict_of_data[file].keys()}
-            for token_id in dict_of_data[file]:
-                token_text = dict_of_data[file][token_id][2]
-                position = dict_of_data[file][token_id][0]
-                full_tag = dict_of_tags[file][token_id]
+        for index in range(len(list_of_tokens)):
+            current_tag = list_of_tags[index]
+            current_token = list_of_tokens[index]
 
-                cur_bilou_tag = full_tag[0:1]
-                cur_common_tag = full_tag[1:]
+            if ne_tag == None and current_tag.startswith('U'):
+                dict_of_final_ne[tuple([current_token])] = current_tag[1:]
 
-                if cur_bilou_tag == 'U':  # если отдельный тег, то мы просто добавляем слово
-                    dic_of_results[tuple([token_id])] = cur_common_tag
+            elif ne_tag == None and current_tag.startswith('B'):
+                ne_tag = current_tag[1:]
+                ne_words.append(current_token)
 
-                elif cur_bilou_tag == 'B' or cur_bilou_tag == 'I':
-                    dic_of_results, ne_tokens, prev_tag = self.tag_b_or_i_case(position, token_position_dic, file,
-                                                                               dict_of_tags, cur_bilou_tag, ne_tokens,
-                                                                               token_id, token_text, dic_of_results,
-                                                                               prev_tag, cur_common_tag)
+            elif ne_tag is not None and 'O' != current_tag:
+                ne_words.append(current_token)
+                if ne_tag in current_tag and current_tag.startswith('L'):
+                    dict_of_final_ne[tuple(ne_words)] = ne_tag
+                    ne_tag = None
+                    ne_words = []
 
-                elif cur_bilou_tag == 'L':
-                    dic_of_results = self.tag_l_case(ne_tokens, token_id, cur_common_tag, dic_of_results)
-                    ne_tokens = []
-                    prev_tag = None
+            elif ne_tag is not None and 'O' == current_tag:
+                dict_of_final_ne[tuple(ne_words)] = ne_tag
+                ne_tag = None
+                ne_words = []
 
-            if ne_tokens:
-                dic_of_results[tuple(ne_tokens)] = prev_tag
-            self.dic_of_files_of_results[file] = dic_of_results
+        logging.log(logging.INFO, dict_of_final_ne)
+        return __to_output_format(dict_of_final_ne)
+    else:
+        raise ValueError('lengths are not equal')
 
-        return self.dic_of_files_of_results
 
-    def tag_b_or_i_case(self, position, token_position_dic, file, dict_of_tags, cur_bilou_tag, ne_tokens, token_id,
-                        token_text, dic_of_results, prev_tag, cur_common_tag):
-        if int(position) == max(list(map(lambda x: int(x), token_position_dic.keys()))):
-            logging.log(logging.ERROR, 'somehow bilou tag B(/I) is the last tag in file ' + str(file))
-        else:
-            sorted_list = sorted(
-                list(map(lambda x: int(x), token_position_dic.keys())))  # отсортированный список позиций
-            next_index = sorted_list.index(int(position)) + 1  # находим индекс следующей позици
-            next_position = sorted_list[next_index]
-            next_token_id = token_position_dic[str(next_position)]
-            next_bilou_tag = dict_of_tags[file][str(next_token_id)][0:1]
-            next_common_tag = dict_of_tags[file][str(next_token_id)][1:]
+def __to_output_format(dict_nes):
+    """
+    :param dict_nes:
+    :return:
+    """
+    list_of_results_for_output = []
 
-            if cur_bilou_tag == 'B':  # если первый тег В
-                dic_of_results, ne_tokens, prev_tag = self.tag_b_case(ne_tokens, dic_of_results, prev_tag,
-                                                                      next_bilou_tag, next_common_tag, cur_common_tag,
-                                                                      token_id, token_text)
+    for tokens_tuple, tag in dict_nes.items():
+        position = tokens_tuple[0].get_position()
+        length = sum([len(token.get_text())+1 for token in tokens_tuple])-1
+        list_of_results_for_output.append([tag, position, length])
 
-            elif cur_bilou_tag == 'I':  # do we need to check some parameters? same type is already checked
-                dic_of_results, ne_tokens, prev_tag = self.tag_i_case(next_bilou_tag, next_common_tag,
-                                                                      cur_common_tag, ne_tokens, token_id,
-                                                                      dic_of_results)
-
-        return dic_of_results, ne_tokens, prev_tag
-
-    def tag_b_case(self, ne_tokens, dic_of_results, prev_tag, next_bilou_tag, next_common_tag, cur_common_tag,
-                   token_id, token_text):
-        if ne_tokens:  # если у нас что-то осталось вne_tokens
-            dic_of_results[tuple(ne_tokens)] = prev_tag  # то мы это добавляем
-            ne_tokens = []  # и ощищаем временное хранилище
-            prev_tag = None
-            if (next_bilou_tag == 'I' or next_bilou_tag == 'L') and next_common_tag == cur_common_tag:
-                prev_tag = cur_common_tag
-                ne_tokens.append(token_id)
-            else:
-                logging.log(logging.ERROR,
-                            'after B (' + token_id + " " + token_text +
-                            ') is not I and L (or I/L of another class)')
-                dic_of_results[tuple([token_id])] = cur_common_tag
-        return dic_of_results, ne_tokens, prev_tag
-
-    def tag_i_case(self, next_bilou_tag, next_common_tag, cur_common_tag, ne_tokens, token_id, dic_of_results):
-        if (next_bilou_tag == 'I' or next_bilou_tag == 'L') and next_common_tag == cur_common_tag:
-            prev_tag = cur_common_tag
-            ne_tokens.append(token_id)
-        else:
-            ne_tokens.append(token_id)
-            dic_of_results[tuple(ne_tokens)] = cur_common_tag
-            ne_tokens = []
-            prev_tag = None
-        return dic_of_results, ne_tokens, prev_tag
-
-    def tag_l_case(self, ne_tokens, token_id, cur_common_tag, dic_of_results):
-        ne_tokens.append(token_id)
-        dic_of_results[tuple(ne_tokens)] = cur_common_tag
-        return dic_of_results
-
-    def writing_to_file(self, filename='testset.txt'):
-        with codecs.open(filename, 'w', encoding='utf-8') as f:
-            pass
-            for file in self.dic_of_files_of_results.keys():
-                f.write(file + '\n')
-                for token in self.dic_of_files_of_results[file]:
-                    f.write(str(list(token)) + ' ' + self.dic_of_files_of_results[file][token] + '\n')
+    return list_of_results_for_output
