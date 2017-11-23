624d23ea13d35918c229d642413e1242cbe460f9
lilaspourpre <nia@ispras.ru>
2017-11-23 11:58:56 +0300

    I tried...

--- src/reader.py
+++ src/reader.py
@@ -3,7 +3,9 @@ import collections
 import os
 import codecs
 import logging
+from collections import OrderedDict
 from src.enitites.document import Document
+from src.enitites.tagged_token import TaggedToken
 from src.enitites.token import Token
 from src.bilou import to_bilou
 
@@ -12,7 +14,7 @@ from src.bilou import to_bilou
 #       Main function
 # ********************************************************************
 
-def get_documents_from(path):
+def get_documents_from(path, search_tags=True):
     """
     Main function for getting documents
     :param path: path to the devset
@@ -21,7 +23,7 @@ def get_documents_from(path):
     dict_of_documents = {}
     filenames = __get_filenames_from(path)
     for filename in filenames:
-        document = __create_document_from(filename)
+        document = __create_document_from(filename, search_tags)
         dict_of_documents[filename] = document
     return dict_of_documents
 
@@ -38,7 +40,7 @@ def __get_filenames_from(path):
     list_of_filenames = []
     for root, dirs, files in os.walk(path):
         for file in files:
-            if file.endswith('.txt'):
+            if file.endswith('.tokens'):
                 list_of_filenames.append(os.path.join(root, os.path.splitext(file)[0]))  # XXX splitext
     logging.log(logging.INFO, "Successfully got filenames from path")
     return list_of_filenames
@@ -48,15 +50,14 @@ def __get_filenames_from(path):
 #       Getting documents
 # -------------------------------------------------------------------
 
-def __create_document_from(filename):
+def __create_document_from(filename, search_tags):
     """
     :param filename: which document to parse (name without extension)
     :return: document class
     """
     tokens = __get_tokens_from(filename)
-    tagged_tokens = __get_tagged_tokens_from(filename, tokens)
+    tagged_tokens = __get_tagged_tokens_from(filename, tokens, search_tags)
     document = Document(tagged_tokens)
-    logging.log(logging.INFO, "SUCCESSFULLY CREATED: document for " + filename)
     return document
 
 
@@ -104,17 +105,21 @@ def __create_token_from(row):
 #       Getting tagged tokens
 #
 
-def __get_tagged_tokens_from(filename, tokens):
+def __get_tagged_tokens_from(filename, tokens, search_tags):
     """
     :param filename: filename without extension (.spans and .objects) to parse
     :param tokens: tokens that need to be tagged
     :return: list of tagged tokens classes
     """
-    span_dict = __spanid_to_tokenids(filename + '.spans',
-                                     [token.get_id() for token in tokens])  # XXX span_to_tokens or spanid_to_tokenids
-    object_dict = __to_dict_of_objects(filename + '.objects')
-    dict_of_nes = __merge(object_dict, span_dict, [token.get_id() for token in tokens])
-    return to_bilou.get_tagged_tokens_from(dict_of_nes, tokens)
+    if search_tags:
+        span_dict = __spanid_to_tokenids(filename + '.spans',
+                                         [token.get_id() for token in
+                                          tokens])  # XXX span_to_tokens or spanid_to_tokenids
+        object_dict = __to_dict_of_objects(filename + '.objects')
+        dict_of_nes = __merge(object_dict, span_dict, tokens)
+        return to_bilou.get_tagged_tokens_from(dict_of_nes, tokens)
+    else:
+        return [TaggedToken(None, tokens[i]) for i in range(len(tokens))]
 
 
 # ___________________________________________________________________________________
@@ -196,13 +201,11 @@ def __get_dict_of_nes(object_dict, span_dict):
     :param span_dict:
     :return:
     """
-    ne_dict = collections.defaultdict(list) # XXX why not use set instead of list?
-
-    # XXX don't use object[0] and object[1]. Just use normal names in for ... in ... 
-    for object in object_dict.items():
-        for span in span_dict.items(): # XXX iterating through ALL spans, but actually needed id's are already known.
-            if span[0] in object[1]['spans']:
-                ne_dict[(object[0], object[1]['tag'])].extend(span[1]) # convert to ints - you'll need them later
+    ne_dict = collections.defaultdict(set)
+    for obj_id, obj_values in object_dict.items():
+        for span in obj_values['spans']:
+            for token in span_dict[span]:
+                ne_dict[(obj_id, obj_values['tag'])].add(token)
     for ne in ne_dict:
         ne_dict[ne] = sorted(list(set([int(i) for i in ne_dict[ne]])))
     return ne_dict
@@ -217,18 +220,21 @@ def __clean(ne_dict, tokens):
     :return:
     """
     sorted_nes = sorted(ne_dict.items(), key=__sort_by_tokens)
-    start_ne = sorted_nes[0] # XXX actually before this checking for non-emptiness is required
     result_nes = {}
-    for ne in sorted_nes:
-        if __intersect(start_ne[1], ne[1]):
-            # XXX it seems you're not dealing with lost tokens (when token id is not specified among named entity tokens)
-            # XXX this probably works only if there was an intersecting token before
-            result_nes[start_ne[0][0]] = {'tokens_list': __check_order([str(i) for i in start_ne[1]], tokens),
-                                          'tag': start_ne[0][1]}
-            start_ne = ne # XXX Oops. Last ne from sorted_nes seems to be just lost. 
-        else:
-            result_tokens_list = __check_normal_form(start_ne[1], ne[1])
-            start_ne = (start_ne[0], result_tokens_list)
+    if len(sorted_nes) != 0:
+        start_ne = sorted_nes[0]
+        for ne in sorted_nes:
+            if __not_intersect(start_ne[1], ne[1]):
+                result_nes[start_ne[0][0]] = {
+                    'tokens_list': __check_order(start_ne[1], tokens),
+                    'tag': start_ne[0][1]}
+                start_ne = ne
+            else:
+                result_tokens_list = __check_normal_form(start_ne[1], ne[1])
+                start_ne = (start_ne[0], result_tokens_list)
+        result_nes[start_ne[0][0]] = {
+            'tokens_list': __check_order(start_ne[1], tokens),
+            'tag': start_ne[0][1]}
     return result_nes
 
 
@@ -237,25 +243,67 @@ def __sort_by_tokens(tokens):
     return (min(ids_as_int), -max(ids_as_int))
 
 
-def __intersect(start_ne, current_ne): # XXX actually this function checks *not intersect*
+def __not_intersect(start_ne, current_ne):
     intersection = set.intersection(set(start_ne), set(current_ne))
     return intersection == set()
 
 
+def __check_normal_form(start_ne, ne):
+    all_tokens = set.union(set(start_ne), set(ne))
+    return __find_all_range_of_tokens(all_tokens)
+
+
+def __find_all_range_of_tokens(tokens):
+    tokens = sorted(tokens)
+    if (tokens[-1] - tokens[0] - len(tokens)) < 5:
+        return list(range(tokens[0], tokens[-1] + 1))
+    else:
+        logging.log(logging.WARNING, 'Difference between ids is too big : '+str(tokens))
+        return tokens
+
+
 def __check_order(list_of_tokens, all_tokens):
+    """
+    :param list_of_tokens:
+    :param all_tokens:
+    :return:
+    """
+    list_of_tokens = [str(i) for i in __find_all_range_of_tokens(list_of_tokens)]
     result = []
+    dict_of_text_by_id = OrderedDict()
+    for i in range(len(all_tokens)):
+        dict_of_text_by_id[all_tokens[i].get_id()] = (all_tokens[i].get_text(), i)
     for token in list_of_tokens:
-        # XXX double linear search!!! "token in all_tokens" and "all_tokens.index(token)"!!! 
-        if token in all_tokens:
-            result.append((token, all_tokens.index(token)))
-    result = sorted(result, key=__sort_by_id) # XXX that is actually sort_by_position
+        try:
+            index_in_all = dict_of_text_by_id[token][1]
+            result.append((token, index_in_all))
+        except KeyError:
+            pass
+    result = sorted(result, key=__sort_by_position)
+    result = add_quotation_marks(result, dict_of_text_by_id)
     return [r[0] for r in result]
 
 
-def __sort_by_id(result_tuple):
+def add_quotation_marks(result, dict_of_text_by_id):
+    """
+    :param result:
+    :param dict_of_text_by_id:
+    :return:
+    """
+    result_tokens_texts = [dict_of_text_by_id[token[0]][0] for token in result]
+    prev_id = [r[1] for r in result][0] - 1
+    next_id = [r[1] for r in result][-1] + 1
+
+    if prev_id >= 0 and list(dict_of_text_by_id.values())[prev_id][0] == '«' \
+            and '»' in result_tokens_texts and '«' not in result_tokens_texts:
+        result = [(list(dict_of_text_by_id.keys())[prev_id], prev_id)] + result
+
+    if next_id < len(dict_of_text_by_id) and list(dict_of_text_by_id.values())[next_id][0] == '»' \
+            and '«' in result_tokens_texts and '»' not in result_tokens_texts:
+        result = result + [(list(dict_of_text_by_id.keys())[next_id], next_id)]
+
+    return result
+
+
+def __sort_by_position(result_tuple):
     return result_tuple[1]
-
-
-def __check_normal_form(start_ne, ne):
-    all_tokens = sorted(set.union(set(start_ne), set(ne)))
-    return list(range(int(all_tokens[0]), int(all_tokens[-1]) + 1)) # XXX we are converting to int all the time - why not do it just once at the beginning?
