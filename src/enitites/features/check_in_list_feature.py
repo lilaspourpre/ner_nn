6879d293ce38fbb7ca3ccae308acec3c42edfef2
lilaspourpre <nia@ispras.ru>
2017-12-06 14:31:42 +0300

    editing CheckInList feature eliminating parameters that are use by morph analyzer (just checking in list)

--- src/enitites/features/check_in_list_feature.py
+++ src/enitites/features/check_in_list_feature.py
@@ -3,39 +3,23 @@ from enitites.features.abstract_feature import AbstractFeature
 
 
 class CheckInListFeature(AbstractFeature):
-    def __init__(self, name, list_of_strings, function=None, return_type=1): # XXX return_type is &^%@$!
+    def __init__(self, name, set_of_strings, forward=True):
         """
         :param name:
-        :param list_of_strings:
-        :param return_type: normal=1 or reverse=0 (0 instead of 1)
-        :param function:
+        :param set_of_strings:
+        :param forward:
         """
         super().__init__()
         self.name = name
-        self.return_type = return_type
-        self.function = function
-        self.strings_with_position = {}
-        for position in range(len(list_of_strings)):
-            self.strings_with_position[list_of_strings[position]] = position
+        self.forward = forward
+        self.strings_with_position = set(set_of_strings)
 
-    # XXX incorrect feature logic
     def compute_vector_for(self, token, document):
-        result = [0] * self.get_vector_size()
-        if self.function:
-            parsed_words = document.get_morpho_parsed_tokens()
-            parsed_word = parsed_words[token.get_id()]
-            what_to_compare = str(eval("parsed_word" + self.function)) # XXX very bad style
-        else:
-            what_to_compare = token.get_text()
-        if what_to_compare in self.strings_with_position:
-            result[self.strings_with_position[what_to_compare]] = 1
-        return self.__check(result)
+        text_token = token.get_text()
+        return self.__check(text_token in self.strings_with_position)
 
     def __check(self, result):
-        if self.return_type == 0:
-            return [int(i == 0) for i in result]
-        else:
-            return result
+        return [int(self.forward == result)]
 
     def get_vector_size(self):
         return len(self.strings_with_position)
--- src/enitites/features/not_in_stop_words.py
+++ src/enitites/features/not_in_stop_words.py
@@ -6,4 +6,4 @@ class StopWordsFeature(CheckInListFeature):
     STOP_WORDS = ('ЖКХ', 'ОБЖ', 'ЭВМ', 'БИО')
 
     def __init__(self, stop_words=STOP_WORDS):
-        super().__init__('stop words', stop_words, return_type=0)
+        super().__init__('stop words', stop_words, forward=False)
