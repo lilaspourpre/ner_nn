d80173f23fde453287425ea4ffe08d68aaacd0f2
lilaspourpre <nia@ispras.ru>
2017-12-08 14:49:38 +0300

    compute counter instead of token text

--- src/enitites/document.py
+++ src/enitites/document.py
@@ -1,21 +1,17 @@
 # -*- coding: utf-8 -*-
+from collections import Counter
+
 import pymorphy2
 from nltk.tokenize import sent_tokenize
 
-# XXX summary of XXXs below: lots of unneeded code was written
 class Document:
     def __init__(self, list_of_tagged_tokens, morph_analyzer=pymorphy2.MorphAnalyzer()):
-        self.morph = morph_analyzer # XXX no need to store it - just pass to __compute_morpho_parsed_tokens
         self.__list_of_tagged_tokens = list_of_tagged_tokens
         self.__list_of_tokens = self.__compute_tokens()
-        self.__dict_of_parsed_tokens = self.__compute_morpho_parsed_tokens()
+        self.__dict_of_parsed_tokens = self.__compute_morpho_parsed_tokens(morph_analyzer)
         self.__list_of_sentences = self.__compute_sentences()
-        self.__token_text_by_id = self.__compute_token_texts_by_id()
-        self.__token_text_by_index = self.__compute_token_texts_by_index()
-        self.__id_by_tokens = self.__compute_id_by_tokens()
         self.__index_by_tokens = self.__compute_index_by_tokens()
-        self.__id_by_index = self.__compute_id_by_index()
-        self.__token_texts = self.__compute_token_texts()
+        self.__counter_token_texts = self.__compute_counter_token_texts()
 
     def get_tagged_tokens(self):
         return self.__list_of_tagged_tokens
@@ -29,27 +25,17 @@ class Document:
     def get_sentences(self):
         return self.__list_of_sentences
 
-    def get_token_texts(self):
-        return self.__token_texts
-
-    # XXX seems unused
-    def get_token_text_by_id(self, token_id):
-        return self.__token_text_by_id[token_id]
-
-    # XXX seems unused
-    def get_token_text_by_index(self, index):
-        return self.__token_text_by_index[index]
-    
-    # XXX seems unused
-    def get_id_by_token(self, token):
-        return self.__id_by_tokens[token]
+    def get_counter_token_texts(self):
+        return self.__counter_token_texts
 
     def get_index_by_token(self, token):
         return self.__index_by_tokens[token]
 
     def get_id_by_index(self, index):
-        # XXX no need to create special map for this operation. Straightforward implementation would be fast enough
-        return self.__id_by_index[index]
+        return self.__list_of_tokens[index].get_id()
+
+    def get_size(self):
+        return len(self.__list_of_tokens)
 
     def __compute_tokens(self):
         tokens = []
@@ -57,32 +43,12 @@ class Document:
             tokens.append(tagged_token.get_token())
         return tokens
 
-    def __compute_morpho_parsed_tokens(self):
+    def __compute_morpho_parsed_tokens(self, morph_analyzer):
         dict_of_parsed_words = {}
         for token in self.__list_of_tokens:
-            parsed_word = self.morph.parse(token.get_text())[0]
+            parsed_word = morph_analyzer.parse(token.get_text())[0]
             dict_of_parsed_words[token.get_id()] = parsed_word
         return dict_of_parsed_words
-
-    def __compute_token_texts_by_id(self):
-        tokens_by_id = {}
-        for i in self.get_tokens():
-            tokens_by_id[i.get_id()] = i.get_text()
-        return tokens_by_id
-
-    def __compute_token_texts_by_index(self):
-        tokens_by_index = {}
-        tokens = self.get_tokens()
-        for i in range(len(tokens)):
-            tokens_by_index[i] = tokens[i].get_text()
-        return tokens_by_index
-
-    def __compute_id_by_tokens(self):
-        id_by_tokens = {}
-        tokens = self.get_tokens()
-        for token in tokens:
-            id_by_tokens[token] = token.get_id()
-        return id_by_tokens
 
     def __compute_index_by_tokens(self):
         index_by_tokens = {}
@@ -91,15 +57,9 @@ class Document:
             index_by_tokens[tokens[i]] = i
         return index_by_tokens
 
-    def __compute_id_by_index(self):
-        id_by_index = {}
-        tokens = self.get_tokens()
-        for i in range(len(tokens)):
-            id_by_index[i] = tokens[i].get_id()
-        return id_by_index
-
-    def __compute_token_texts(self):
-        return list(self.__token_text_by_id.values())
+    def __compute_counter_token_texts(self):
+        list_of_text_tokens = [token.get_text() for token in self.get_tokens()]
+        return Counter(list_of_text_tokens)
 
     # XXX actually, this info is within input file. No need to recompute it, especially with nltk
     def __compute_sentences(self):
