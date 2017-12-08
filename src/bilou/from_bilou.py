167417efc49e8ebb8e95a39aa421ead4ea0f73f0
lilaspourpre <nia@ispras.ru>
2017-12-08 14:42:44 +0300

    from bilou revised

--- src/bilou/from_bilou.py
+++ src/bilou/from_bilou.py
@@ -17,11 +17,14 @@ def untag(list_of_tags, list_of_tokens):
                 current_tag = list_of_tags[index]
                 current_token = list_of_tokens[index]
 
-                if current_tag.startswith('B') or current_tag.startswith('I') or current_tag.startswith('L'):
-                    dict_of_final_ne, ne_words, ne_tag = __check_bil(dict_of_final_ne, ne_words, ne_tag, current_tag,
+                if current_tag.startswith('B') or current_tag.startswith('I'):
+                    dict_of_final_ne, ne_words, ne_tag = __check_bi(dict_of_final_ne, ne_words, ne_tag, current_tag,
+                                                                     current_token)
+                elif current_tag.startswith('L'):
+                    dict_of_final_ne, ne_words, ne_tag = __check_l(dict_of_final_ne, ne_words, ne_tag, current_tag,
                                                                      current_token)
                 elif current_tag.startswith('O'):
-                    dict_of_final_ne, ne_words, ne_tag = __check_o(dict_of_final_ne, ne_words, ne_tag)
+                    dict_of_final_ne, ne_words, ne_tag = __finish_ne_if_required(dict_of_final_ne, ne_words, ne_tag)
 
                 elif current_tag.startswith('U'):
                     dict_of_final_ne, ne_words, ne_tag = __check_u(dict_of_final_ne, ne_words, ne_tag, current_tag,
@@ -34,56 +37,51 @@ def untag(list_of_tags, list_of_tokens):
                 else:
                     raise Exception('Somehow ne_words is None and ne_tag is not None')
 
-        if ne_words != [] and ne_tag is not None:
-            dict_of_final_ne[tuple(ne_words)] = ne_tag
+        dict_of_final_ne, ne_words, ne_tag = __finish_ne_if_required(dict_of_final_ne, ne_words, ne_tag)
         return __to_output_format(dict_of_final_ne)
     else:
         raise ValueError('lengths are not equal')
 
 
-def __check_bil(dict_of_final_ne, ne_words, ne_tag, current_tag, current_token):
+def __check_bi(dict_of_final_ne, ne_words, ne_tag, current_tag, current_token):
     if ne_tag is None and ne_words == []:
-        # XXX incorrect logic for L - just need to finish NE, but it starts a new one
         ne_tag = current_tag[1:]
         ne_words = [current_token]
-
     else:
-        if current_tag.startswith('B'):
+        if current_tag.startswith('I') and ne_tag == current_tag[1:]:
+            ne_words.append(current_token)
+        else:
             dict_of_final_ne, ne_words, ne_tag = __replace_by_new(dict_of_final_ne, ne_words, ne_tag, current_tag,
                                                                   current_token)
-        else:
-            if ne_tag == current_tag[1:]:
-                ne_words.append(current_token)
-                if current_tag.startswith('L'):
-                    # XXX looks like code from __check_o
-                    dict_of_final_ne[tuple(ne_words)] = ne_tag
-                    ne_tag = None
-                    ne_words = []
-            else:
-                dict_of_final_ne, ne_words, ne_tag = __replace_by_new(dict_of_final_ne, ne_words, ne_tag, current_tag,
-                                                                      current_token)
     return dict_of_final_ne, ne_words, ne_tag
 
 
-def __replace_by_new(dict_of_final_ne, ne_words, ne_tag, current_tag, current_token):
-    # XXX partially duplicates __check_o logic
-    dict_of_final_ne[tuple(ne_words)] = ne_tag
-    ne_tag = current_tag[1:]
-    ne_words = [current_token]
-    return dict_of_final_ne, ne_words, ne_tag
-
-# XXX I'd rename to something like __finish_ne_if_required
-def __check_o(dict_of_final_ne, ne_words, ne_tag):
-    if ne_tag is not None and ne_words != []:
-        dict_of_final_ne[tuple(ne_words)] = ne_tag
-        ne_tag = None
-        ne_words = []
+def __check_l(dict_of_final_ne, ne_words, ne_tag, current_tag, current_token):
+    if ne_tag == current_tag[1:]:
+        dict_of_final_ne, ne_words, ne_tag = __finish_ne_if_required(dict_of_final_ne, ne_words+[current_token], ne_tag)
+    else:
+        dict_of_final_ne, ne_words, ne_tag = __finish_ne_if_required(dict_of_final_ne, ne_words, ne_tag)
+        dict_of_final_ne, ne_words, ne_tag = __finish_ne_if_required(dict_of_final_ne, [current_token], current_tag[1:])
     return dict_of_final_ne, ne_words, ne_tag
 
 
 def __check_u(dict_of_final_ne, ne_words, ne_tag, current_tag, current_token):
-    dict_of_final_ne, ne_words, ne_tag = __check_o(dict_of_final_ne, ne_words, ne_tag)
-    dict_of_final_ne[tuple([current_token])] = current_tag[1:]
+    dict_of_final_ne, ne_words, ne_tag = __finish_ne_if_required(dict_of_final_ne, ne_words, ne_tag)
+    return __finish_ne_if_required(dict_of_final_ne, [current_token], current_tag[1:])
+
+
+def __replace_by_new(dict_of_final_ne, ne_words, ne_tag, current_tag, current_token):
+    dict_of_final_ne, ne_words, ne_tag = __finish_ne_if_required(dict_of_final_ne, ne_words, ne_tag)
+    ne_tag = current_tag[1:]
+    ne_words = [current_token]
+    return dict_of_final_ne, ne_words, ne_tag
+
+
+def __finish_ne_if_required(dict_of_final_ne, ne_words, ne_tag):
+    if ne_tag is not None and ne_words != []:
+        dict_of_final_ne[tuple(ne_words)] = ne_tag
+        ne_tag = None
+        ne_words = []
     return dict_of_final_ne, ne_words, ne_tag
 
 
