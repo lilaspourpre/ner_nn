f6405e13c9dde3dd694b285034b68ad2f8e29211
Irina Nikishina <nia@ispras.ru>
2018-03-30 22:00:36 +0300

    add new conv2d still bug

--- src/machine_learning/cnn.py
+++ src/machine_learning/cnn.py
@@ -21,24 +21,59 @@ class CNN():
             max_pool_outputs = tf.nn.max_pool(conv_outputs, [1, f_size, 1, 1], [1, 1, 1, 1], 'SAME')
             return tf.squeeze(max_pool_outputs, [2])
 
-        self.conv1 = apply_conv(self.filter_sizes[-1], self.x_new, self.input_size)
+        def apply_conv2(f_size, input_tensor, o_size):
+            W = tf.Variable(tf.truncated_normal([f_size, self.input_size, 1, o_size], stddev=0.1), name="W")
+            b = tf.Variable(tf.constant(0.1, shape=[o_size]), name="b")
+            conv = tf.nn.conv2d(
+                input_tensor,
+                W,
+                strides=[1, 1, self.input_size, 1],
+                padding="SAME")
+
+            # Apply nonlinearity
+            h = tf.nn.relu(tf.nn.bias_add(conv, b), name="relu")
+            # Max-pooling over the outputs
+            pooled = tf.nn.max_pool(
+                h,
+                ksize=[1, f_size, 1, 1],
+                strides=[1, 1, 1, 1],
+                padding='SAME',
+                name="pool")
+            return tf.squeeze(pooled, [2])
+
+        self.conv1 = apply_conv2(self.filter_sizes[-1], self.x_new, self.input_size)
         self.conv1_new = tf.expand_dims(self.conv1, -1)
 
         for i in range(len(self.filter_sizes)):
             if i == 0:
-                self.conv2 = apply_conv(self.filter_sizes[i], self.conv1_new, self.hidden_size)
+                self.conv2 = apply_conv2(self.filter_sizes[i], self.conv1_new, self.hidden_size)
             else:
                 self.conv2 = tf.concat([self.conv2,
-                                        apply_conv(self.filter_sizes[i], self.conv1_new, self.hidden_size)], -1)
+                                        apply_conv2(self.filter_sizes[i], self.conv1_new, self.hidden_size)], -1)
 
         self.outputs = tf.contrib.layers.fully_connected(self.conv2, self.output_size, activation_fn=tf.tanh)
+
         mask = tf.sequence_mask(
-            self.seqlen,
-            dtype=tf.float32)
+             self.seqlen,
+             dtype=tf.float32)
 
         self.cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=self.outputs, labels=self.y)
         self.loss = (tf.reduce_sum(self.cross_entropy * mask) / tf.cast(tf.reduce_sum(self.seqlen), tf.float32))
-        self.train = tf.train.AdamOptimizer(learning_rate=0.01).minimize(self.loss)
+
+
+        self.train = tf.train.AdamOptimizer(learning_rate=0.001).minimize(self.loss)
+
+        #global_step = tf.Variable(0, name="global_step", trainable=False)
+        #optimizer = tf.train.AdamOptimizer(0.001)
+        #grads_and_vars = optimizer.compute_gradients(self.loss)
+        #self.train = optimizer.apply_gradients(grads_and_vars, global_step=global_step)
+
+        # self.params = tf.trainable_variables()
+        # self.gradients = tf.gradients(self.loss, self.params)
+        # self.clipped_gradients, _ = tf.clip_by_global_norm(self.gradients, 1)
+        #
+        # self.optimizer = tf.train.AdamOptimizer(learning_rate=0.001)
+        # self.train = self.optimizer.apply_gradients(zip(self.clipped_gradients, self.params))
 
         self.init = tf.global_variables_initializer()
         self.sess = tf.Session()
--- src/main.py
+++ src/main.py
@@ -126,7 +126,7 @@ def choose_model(method, window, train_documents, test_documents, ngram_affixes,
         tags = compute_tags()
         feature = get_composite_feature(window, train_documents, ngram_affixes, embedding_model)
         cnn = CNN(input_size=int(feature.get_vector_size()), output_size=len(tags), hidden_size=100, batch_size=8)
-        return CNNTrainer(epoch=100, nn=cnn, tags=tags), feature
+        return CNNTrainer(epoch=10, nn=cnn, tags=tags), feature
     else:
         raise argparse.ArgumentTypeError(
             'Value has to be "majorclass" or "random" or "svm" or "ml_pc" or "lstm" or "bilstm" or "cnn"')
@@ -139,7 +139,7 @@ def get_composite_feature(window, train_documents, ngram_affixes, embedding_mode
     """
     list_of_features = [LengthFeature(), NumbersInTokenFeature(), PositionFeature(), DFFeature(), ConcordCaseFeature(),
                         GazetterFeature(), LowerCaseFeature(), SpecCharsFeature(),
-                        StopWordsFeature(), EmbeddingFeature(embedding_model)]
+                        StopWordsFeature()] #, EmbeddingFeature(embedding_model)]
 
     list_of_features.append(
         __compute_affixes(PrefixFeature, ngram_affixes, train_documents, end=ngram_affixes))
